218345543c905fe7ad7de05d7c5a2acd8aecf40e
Florin Godard <fgodard@gmail.com>
2016-07-21 18:30:45 +0200

    First working version, readability based on string length

--- index.php
+++ index.php
@@ -1,143 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="favicon.ico">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="favicon.ico">
 
-    <title>wasmitnetzen.de | wikipedia-readability</title>
+		<title>wasmitnetzen.de | wikipedia-readability</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap theme -->
-    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<!-- Bootstrap theme -->
+		<link href="css/bootstrap-theme.min.css" rel="stylesheet">
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="theme.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+		<link href="theme.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+		<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+		<!--[if lt IE 9]><script src="assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+		<script src="assets/js/ie-emulation-modes-warning.js"></script>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
 
-  <body role="document">
+	<body role="document">
 
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">wikipedia-readability</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="index.php">Home</a></li>
-            <li><a href="https://keybase.io/wasmitnetzen">Keybase.io</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+		<!-- Fixed navbar -->
+		<nav class="navbar navbar-inverse navbar-fixed-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">wikipedia-readability</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.php">Home</a></li>
+						<li><a href="https://keybase.io/wasmitnetzen">Keybase.io</a></li>
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</nav>
 
-    <div class="container theme-showcase" role="main">
+		<div class="container theme-showcase" role="main">
 
-      <!-- Main jumbotron for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1>wikipedia-readability</h1>
-        <p>This is my approach at the Task2 from <a href="https://www.mediawiki.org/wiki/User:Kaldari/Task_2">User:Kaldari</a> at Mediawiki.org.</p>
-        <p>The code can be found on <a href="https://github.com/wasmitnetzen/wikipedia-readability">Github</a> and it is licenced under the MIT License.</p>
-      </div>
-      <?php
-      if (!isset($_GET["category"])) { ?>
-      <div class="page-header">
-        <h1>What To Do?</h1>
-      </div>
-      <div class="well">
-        <p>This tool fetches all articles from a given category at the English Wikipedia, calculates a readability score on them and shows a sorted result below.</p><p>Please start by giving a category name in the form below.</p>
-        <p><form action="index.php" method="GET">
-          <table style="width: 90%">
-            <tr>
-              <td><b>Category name:</b></td>
-              <td><input type="text" name="category" style="width:70%" /></td>
-            </tr>
-          </table>
-          <input type="submit" class="btn btn-default" value="Send form" />
-        </form></p>
-      </div>
-      <?php } else { ?>
-      <div class="page-header">
-        <h1>Results</h1>
-      </div>
-      <?php
-      include("readabilityAnalyzer.php");
-      $readabilityAnalyzer = new ReadabilityAnalyzer();
-      $readabilityAnalyzer->buildResults($_GET["category"]);
-      ?>
-      <div>
-        <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td rowspan="2">1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@TwBootstrap</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
-          <a href="index.php">&lt; Back</a>
-          </div>
-      <?php } ?>
-    </div> <!-- /container -->
+			<!-- Main jumbotron for a primary marketing message or call to action -->
+			<div class="jumbotron">
+				<h1>wikipedia-readability</h1>
+				<p>This is my approach at the Task2 from <a href="https://www.mediawiki.org/wiki/User:Kaldari/Task_2">User:Kaldari</a> at Mediawiki.org.</p>
+				<p>The code can be found on <a href="https://github.com/wasmitnetzen/wikipedia-readability">Github</a> and it is licenced under the MIT License.</p>
+			</div>
+			<?php
+			if (!isset($_GET["category"])) { ?>
+			<div class="page-header">
+				<h1>What To Do?</h1>
+			</div>
+			<div class="well">
+				<p>This tool fetches all articles from a given category at the English Wikipedia, calculates a readability score on them and shows a sorted result below.</p><p>Please start by giving a category name in the form below.</p>
+				<p><form action="index.php" method="GET">
+					<table style="width: 90%">
+						<tr>
+							<td><b>Category name:</b></td>
+							<td><input type="text" name="category" style="width:70%" /></td>
+						</tr>
+					</table>
+					<input type="submit" class="btn btn-default" value="Send form" />
+				</form></p>
+			</div>
+			<?php } else { ?>
+			<div class="page-header">
+				<h1>Results</h1>
+			</div>
+			<div>
+				<?php
+				include("readabilityAnalyzer.php");
+				$readabilityAnalyzer = new ReadabilityAnalyzer();
+				echo $readabilityAnalyzer->buildResults($_GET["category"]);
+				?>
+
+				<a href="index.php">&lt; Back</a>
+			</div>
+			<?php } ?>
+		</div> <!-- /container -->
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="assets/js/docs.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="assets/js/docs.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+		<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+	</body>
 </html>
--- readabilityAnalyzer.php
+++ readabilityAnalyzer.php
@@ -1,14 +1,25 @@
 <?php
 
+
+
 class ReadabilityAnalyzer {
 	var $endpoint = "https://en.wikipedia.org/w/api.php?action=query&format=json&utf8=1";
 	var $categoryNamespace = "Category";
 	var $curl;
+	var $curlLog;
 
 	function __construct() {
 		$this->curl = curl_init();
 		curl_setopt($this->curl, CURLOPT_USERAGENT, 'wikipedia-readability/1.0 (https://wasmitnetzen.de/wikipedia-readability; irgend-wikipedia-readability@wasmitnetzen.de)');
 		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($this->curl, CURLOPT_VERBOSE, 1);
+
+		$this->curlLog = fopen("curl.log", 'w');
+		curl_setopt($this->curl, CURLOPT_STDERR, $this->curlLog);
+	}
+
+	function  __destruct() {
+		fclose($this->curlLog);
 	}
 
 	function buildResults($category) {
@@ -21,20 +32,50 @@ class ReadabilityAnalyzer {
 		$categoryContent = json_decode($this->fetchCategory($category));
 		// parse JSON
 		$categoryMembers = $categoryContent->query->categorymembers;
+		$categoryMembersCount = count($categoryMembers);
 		#var_dump($categoryMembers);
-		$readabilityArray = array();
+		$pageIds = "";
+		$articleCount = 0;
+		$extractArray = array();
 		foreach ($categoryMembers as $article) {
 			// fetch article
 			#var_dump($article);
-			$pageid = $article->pageid;
+			$pageId = $article->pageid;
 			$title =  $article->title;
-			echo "Handling $title ($pageid)<br>";
-			// parse JSON
-			$articleResponse = json_decode($this->fetchArticle($pageid));
-			// calculate readability
-			$readability = $this->calculateReadbility($articleContent);
+			#echo "Handling $title ($pageId)<br>";
+			$pageIds .= $pageId."|";
+			$articleCount++;
+			if ($articleCount % 20 == 0 || $articleCount == $categoryMembersCount) {
+				// cut off last |
+				$pageIds = substr($pageIds,0,-1);
+				$articleResponse = json_decode($this->fetchArticles($pageIds));
+				$articles = $articleResponse->query->pages;
+
+				foreach ($articles as $key => $value) {
+					$extractArray[$key] = $value;
+				}
+				$pageIds = "";
+			}
 		}
+		echo "Queried for $categoryMembersCount articles, got ".count($extractArray).".<br>";
+		if ($categoryMembersCount !== count($extractArray)) {
+			echo "Not all articles were returned by API.<br>";
+		}
+		#var_dump($extractArray);
+
+
+		// calculate readability
+		$extractArray = $this->calculateReadbilities($extractArray);
+
+		// sort by readability
+		usort($extractArray, 'readabilitySort');
 		// build table
+		$table = '<table class="table table-bordered"><thead><tr><th>PageId</th><th>Title</th><th>Readability Score</th><th>Extract</th></tr></thead><tbody>';
+		foreach ($extractArray as $index => $article) {
+			$table .= "<tr><td>".$article->pageid."</td><td>".$article->title."</td><td>".$article->readabilityScore."</td><td>".substr($article->extract, 0, 60)."...</td></tr>";
+		}
+		$table .= '</tbody></table>';
+		return $table;
 	}
 
 	function fetchCategory($category) {
@@ -44,13 +85,29 @@ class ReadabilityAnalyzer {
 		return $data;
 	}
 
-	function fetchArticle($pageid) {
-
+	function fetchArticles($pageIds) {
+		#echo "Querying for ".$pageIds." articles.<br>";
+		curl_setopt($this->curl, CURLOPT_URL, $this->endpoint."&prop=extracts&explaintext=1&exchars=10000&exintro=1&exlimit=20&pageids=".$pageIds);
+		$data = curl_exec($this->curl);
+		#echo $data."<br>";
+		return $data;
 	}
 
-	function calculateReadbility($articleContent) {
-
+	function calculateReadbilities($extractArray) {
+		foreach ($extractArray as $key => $extract) {
+			$extract->readabilityScore = $this->calculateReadbilityOnExtract($extract->extract);
+		}
+		return $extractArray;
 	}
+
+	function calculateReadbilityOnExtract($extract) {
+		return strlen($extract);
+	}
+}
+
+function readabilitySort($a, $b)
+{
+    return $a->readabilityScore > $b->readabilityScore;
 }
 
 ?>
